e04f8ec6f24397e696d1d8dc51975c8d93823fba
nilavrusty <neelabhdutta01@gmail.com>
2020-02-05 03:37:52 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const userRouter = require ('./routers/user');
 const taskRouter = require ('./routers/task');
 
 const app = express ();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.use (cors ());
 app.use (express.json ());
 app.use (userRouter);
